23072291053968be0fc9dd8f3b007de3f6f6af40
渡邊丈洋 <take-watanabe@autobacs.com>
2024-04-19 09:27:46 +0900

    Java ConfigでのDI

---
+++ src/main/java/com/example/demo/service/impl/TestLogicImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.BusinessLogic;
+
+public class TestLogicImpl implements BusinessLogic {
+    @Override
+    public void doLogic() {
+        System.out.println("This is TEST.");
+    }
+}
