ac10c684f3ab87452b49dc328c0ea92117fa3b4d
渡邊丈洋 <take-watanabe@autobacs.com>
2024-04-23 17:30:00 +0900

    URLに埋め込まれた値を受け取る

---
+++ src/main/java/com/example/demo/controller/PathValiableController.java
@@ -0,0 +1,47 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class PathValiableController {
+    // GETかつ[url: /show]
+    @GetMapping("show01")
+    public String showView01() {
+        return "show01";
+    }
+    // GETかつ[/function/{no}]
+    @GetMapping("/function/{no}")
+    public String selectFuntion(@PathVariable Integer no) {
+        String view = null;
+        switch (no) {
+            case 1:
+                view = "pathvaliable/function1";
+                break;
+            case 2:
+                view = "pathvaliable/function2";
+                break;
+            case 3:
+                view = "pathvaliable/function3";
+                break;
+        }
+        return view;
+    }
+
+    @PostMapping(value = "send", params = "a")
+    public String showAView() {
+        return "submit/a";
+    }
+
+    @PostMapping(value = "send", params = "b")
+    public String showBView() {
+        return "submit/b";
+    }
+
+    @PostMapping(value = "send", params = "c")
+    public String showCView() {
+        return "submit/c";
+    }
+}
